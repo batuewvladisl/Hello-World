6e2445fb4c13998e1c13099a1296432fecdbe4cd
Batuev Vladislav <batuew.vladisl@yandex.ru>
2021-05-20 10:46:10 +0400

    commit2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,9 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        String hello = "Hello";
-        String world = "World";
-        System.out.println(hello + " " + world + "!");
+        System.out.println("Hello World!");
 
 
     }
